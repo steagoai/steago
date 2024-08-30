55b6ed9f3d63612db71f9d09141307d28255f154
Augustus D'Souza <augustus@clergo.com>
2024-08-30 18:33:08 +0530

    Add next config to web

---
+++ apps/web/next.config.js
@@ -0,0 +1,29 @@
+//@ts-check
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { composePlugins, withNx } = require('@nx/next');
+// const path = require('path');
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+/**
+ * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
+ **/
+const nextConfig = {
+  nx: {
+    // Set this to true if you would like to use SVGR
+    // See: https://github.com/gregberge/svgr
+    svgr: false,
+  },
+  transpilePackages: ['@steago/nexus', '@steago/ui'],
+};
+
+const plugins = [
+  // Add more Next.js plugins to this list if needed.
+  withNx,
+  withBundleAnalyzer,
+];
+
+module.exports = composePlugins(...plugins)(nextConfig);
