79b750ed597c368f5b72b64d9ce02c05dfdd968f
Augustus D'Souza <augustus@clergo.com>
2024-09-03 23:21:04 +0530

    Update next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,7 +2,7 @@
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { composePlugins, withNx } = require('@nx/next');
-// const path = require('path');
+const path = require('path');
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
@@ -18,6 +18,28 @@ const nextConfig = {
     svgr: false,
   },
   transpilePackages: ['@steago/nexus', '@steago/ui'],
+  webpack: (config) => {
+    // Just an attempt at webpack caching to speed up builds, ignore...
+    // config.cache = {
+    //   type: 'filesystem',
+    //   cacheDirectory: path.resolve('./.webpack_cache'),
+    // };
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      react: path.resolve('../../node_modules/react'),
+      'react-dom': path.resolve('../../node_modules/react-dom'),
+      '@tiptap/react': path.resolve('../../node_modules/@tiptap/react'),
+      '@tiptap/pm': path.resolve('../../node_modules/@tiptap/pm'),
+      '@tanstack/react-query': path.resolve(
+        '../../node_modules/@tanstack/react-query'
+      ),
+      '@headlessui/react': path.resolve('../../node_modules/@headlessui/react'),
+      '@steago/nexus': path.resolve('../../node_modules/@steago/nexus/src'),
+      '@steago/ui': path.resolve('../../node_modules/@steago/ui/src'),
+      // "..." // all shared deps in this array
+    };
+    return config;
+  },
 };
 
 const plugins = [
